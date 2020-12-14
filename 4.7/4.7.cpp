a770e2f712b131755807c86d569026d47f648b53
SenyshynM <73256299+SenyshynM@users.noreply.github.com>
2020-12-14 10:13:37 +0200

    1

---
+++ 4.7/4.7.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double xp, xk, x, dx, eps, a = 0, R = 0, S = 0, arth;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "arth(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	
+	
+	x = xp;
+	while (x <= xk)
+	{
+		n = 0; 
+		a = x;
+		S = x;
+		arth = log((1 + x) / (1 - x)) / 2;
+		do {
+			n++;
+			R = x * x * (2 * n - 1) / (2 * n + 1);
+			a *= R;
+			S += a;
+		} while (abs(a) >= eps);
+		
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << arth << " |"
+			<< setw(10) << setprecision(5) << S << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
